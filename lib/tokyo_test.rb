a77f6261bce074feac06f58cd3cbb91810ac62e2
Flinn <flinn@actsasflinn.com>
2009-10-17 01:02:10 -0400

    use ruby-tokyotyrant c client

--- lib/tokyo_test.rb
+++ lib/tokyo_test.rb
@@ -1,27 +1,29 @@
 require 'benchmark'
 require 'rubygems'
-require 'rufus/tokyo/tyrant'
-puts "Using rufus-tokyo #{Rufus::Tokyo::VERSION}"
+require 'tokyo_tyrant'
+puts "Using ruby-tokyotyrant 0.2.0"
 
 def init_connection
-  @db = Rufus::Tokyo::TyrantTable.new('localhost', 41414)
+  @db = TokyoTyrant::Table.new('127.0.0.1', 1978)
   @db.clear
   @db.set_index 'age', :decimal
 end
 
-def create_lots_of_documents(n=200_000)
-  # Tokyo does not appear to have a bulk load operation
-  # so we insert one at a time.
+def create_lots_of_documents(n=200_000, batch_size=1000)
   count = 0
-  while count < n
-    @db["bob#{count}"] = { 
-      'name' => 'Bob Jones',
-      'email' => "bob#{rand(1000)}@example.com", 
-      'age' => rand(100), 
-      'birthdate' => rand(1_000_000_000), 
-      'is_admin?' => rand(2),
-    }
-    count += 1
+  (n / batch_size).times do
+    docs = {}
+    batch_size.times do
+      docs["bob#{count}"] = {
+        'name' => 'Bob Jones',
+        'email' => "bob#{rand(1000)}@example.com",
+        'age' => rand(100),
+        'birthdate' => Time.at(rand(1_000_000_000)),
+        'is_admin?' => rand(2) == 1,
+      }
+      count += 1
+    end
+    @db.mput(docs)
   end
 end
 
@@ -36,9 +38,9 @@ end
 
 def bulk_delete_documents
   before_count = @db.size
-  @db.query_delete do |q|
+  @db.prepare_query do |q|
     q.add_condition 'age', :numge, '80'
-  end
+  end.delete
   after_count = @db.size
   raise ArgumentError, "Unexpected delete result: #{before_count} #{after_count}" if before_count == after_count
 end
